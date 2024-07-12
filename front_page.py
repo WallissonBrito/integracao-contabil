be1ec9b254a51b21c4588ceadee8c04e6608bf13
Aderbal Wallisson de Brito Silva <aderbal.wal@gmail.com>
2024-07-12 13:47:10 -0300

    Switch pages

--- front_page.py
+++ front_page.py
@@ -7,10 +7,23 @@ from ui_index import Ui_MainWindow
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        
+        self.setupUi(self)        
+        self.load_data_page_btn.setChecked(True)
         # load file
         self.df = self.upload_btn.clicked.connect(self.load_data_into_table)
+
+        # Switch pages
+        self.load_data_page_btn.clicked.connect(self.switch_to_load_data_page)
+        self.update_column_page_btn.clicked.connect(self.switch_to_update_column_page)
+        self.add_period_page_btn.clicked.connect(self.switch_to_add_period_page)
+        self.update_accounts_page_btn.clicked.connect(self.switch_to_update_accounts)
+        self.exportation_page_btn.clicked.connect(self.switch_to_exportation_page)
+
+        # Next page button
+        self.next_btn_load_data_page.clicked.connect(self.switch_to_update_column_page)
+        self.next_btn_update_column_page.clicked.connect(self.switch_to_add_period_page)
+        self.next_btn_add_data_page.clicked.connect(self.switch_to_update_accounts)
+        self.next_btn_update_accounts_page.clicked.connect(self.switch_to_exportation_page)
 
     def separator_file(self):
         file_delimiter = self.comboBox_file_delimiter.currentText()
@@ -22,20 +35,22 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             
     def load_file(self):
         # Get the separator
-        delimiter_data = str(self.separator_file())
+        delimiter_data = self.separator_file()
         
         # Store the fila_path
         file_path, _ = QFileDialog.getOpenFileName(self, "Selecione o arquivo", "", "CSV files (*.csv)")
         
         # Read the file
         if file_path:
-            data = pd.read_csv(file_path, sep=delimiter_data)             
+            data = pd.read_csv(file_path, sep=str(delimiter_data))             
             return data
 
     def load_data_into_table(self):
         data = self.load_file()
         # Read columns name
         columns = data.columns
+        self.tableWidget_load_data_page.setColumnCount(len(columns))
+
         # Set columns name into the table
         self.tableWidget_load_data_page.setHorizontalHeaderLabels(columns)
         # Insert data into the table
@@ -44,3 +59,22 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             for col_index, cell_data in enumerate(row_data):
                 item = QTableWidgetItem(str(cell_data))
                 self.tableWidget_load_data_page.setItem(row_index,col_index, item)
+
+    def switch_to_load_data_page(self):
+        self.stackedWidget.setCurrentIndex(0)
+
+    def switch_to_update_column_page(self):
+        self.stackedWidget.setCurrentIndex(1)
+        self.update_column_page_btn.setChecked(True)
+        
+    def switch_to_add_period_page(self):
+        self.stackedWidget.setCurrentIndex(2)
+        self.add_period_page_btn.setChecked(True)
+
+    def switch_to_update_accounts(self):
+        self.stackedWidget.setCurrentIndex(3)
+        self.update_accounts_page_btn.setChecked(True)
+
+    def switch_to_exportation_page(self):
+        self.stackedWidget.setCurrentIndex(4)
+        self.exportation_page_btn.setChecked(True)
